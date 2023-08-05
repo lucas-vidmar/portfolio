9cabc86cac378f43755e3e4783b2c7bfdd7d5818
Lucas Vidmar <lucas@vidmar.xyz>
2023-08-05 18:59:58 -0300

    adding angular fire

---
+++ create_enviroment_file.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+const dir = "src/environments";
+const filename = "environment.ts";
+const prodfilename = "environment.prod.ts";
+
+const envConfigFile = `{process.env.FIREBASE_DETAILS}`;
+
+fs.access(dir, fs.constants.F_OK, (err) => {
+    if (err) {
+        // Directory doesnt exist
+        console.log("Directory doesn't exist, creating", process.cwd());
+        // Create the directory
+        fs.mkdirSync(dir, { recursive: true }, (err) => {
+            if (err) throw err;
+        });
+    }
+    // Directory exists, write file
+    try {
+        fs.writeFileSync(path.join(dir, filename), envConfigFile);
+        fs.writeFileSync(path.join(dir, prodfilename), envConfigFile);
+        console.log("Environment file created successfully in ", process.cwd());
+        if(fs.existsSync(dir + "/"+ filename)) {
+            console.log("Environment was created in ", path.resolve(dir + "/"+ filename));
+            const str = fs.readFileSync(dir + "/"+ filename, 'utf8').toString();
+            console.log(str);
+        }
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import { ProfileComponent } from './profile/profile.component';
 import { BlogEntryComponent } from './blog-entry/blog-entry.component';
 import { EditorComponent } from './editor/editor.component';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -22,8 +28,16 @@ import { NotfoundComponent } from './notfound/notfound.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ]
 })
 export class AppModule { }
