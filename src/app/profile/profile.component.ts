2affd871addaf39638c3752e36bb9b879b88bfe5
Lucas Vidmar <lucas@vidmar.xyz>
2023-09-22 18:27:13 -0300

    profile layout

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  tags = [
+    "Python",
+    "HTML",
+    "CSS",
+    "JS",
+    "TS",
+    "Angular",
+    "C",
+    "C++",
+    "C#",
+    "Java",
+    "C#",
+    "OSPF",
+    "BGP",
+    "MPLS",
+    "IS-IS",
+    "RIP",
+    "PM/PO",
+    "Sales",
+    "CI/CD",
+    "Docker",
+    "Kubernetes",
+    "Ansible",
+    "Terraform",
+    "AWS",
+    "GCP",
+    "Azure",
+    "Assembly",
+    "Bash",
+    "PowerShell",
+    "Linux",
+    "SolidWorks",
+    "AutoCAD"
+  ]
+
+  constructor() {
+    // Randomize tags
+    this.tags.sort(() => Math.random() - 0.5);
+  }
+
 }
